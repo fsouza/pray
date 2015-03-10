640ab5577df9b9aabdd9d3966c256b9d4eeccb5b
Francisco Souza <f@souza.cc>
2015-03-09 23:26:23 -0300

    pray: add godoc to the command

--- pray.go
+++ pray.go
@@ -2,6 +2,25 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// pray is a tool for finding unused public items (functions, methods, types,
+// variables and constants) in packages, searching in a given context (the
+// context being a list of packages).
+//
+// Example of use:
+//
+//     % pray -src github.com/tsuru/config github.com/tsuru/tsuru/app
+//     /Users/f/src/github.com/tsuru/config/checker.go:10:6: Check is unused
+//     /Users/f/src/github.com/tsuru/config/config.go:113:6: WriteConfigFile is unused
+//     /Users/f/src/github.com/tsuru/config/config.go:229:6: GetUint is unused
+//     /Users/f/src/github.com/tsuru/config/config.go:75:6: ReadAndWatchConfigFile is unused
+//     /Users/f/src/github.com/tsuru/config/config.go:296:6: GetList is unused
+//     /Users/f/src/github.com/tsuru/config/config.go:208:6: GetFloat is unused
+//
+// The flag -src specifies the source packages, and then users can provide the
+// set of packages for searching. Users can also use the Go notation for
+// expanding subpackages:
+//
+//     % pray -src github.com/tsuru/config github.com/tsuru/tsuru/...
 package main
 
 import (
